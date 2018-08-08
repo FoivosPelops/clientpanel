c7cdae816c51a6364f978afaa1c5596426a138f1
foivos pelopidas <it1463@uom.edu.gr>
2018-08-08 14:09:10 +0300

    first time push

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyA6g_qZW7WtEw0YKlsf_L8TPoLYGU45a4w",
+    authDomain: "clientpanelprod-cb002.firebaseapp.com",
+    databaseURL: "https://clientpanelprod-cb002.firebaseio.com",
+    projectId: "clientpanelprod-cb002",
+    storageBucket: "clientpanelprod-cb002.appspot.com",
+    messagingSenderId: "1065449154786"
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,16 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyA6g_qZW7WtEw0YKlsf_L8TPoLYGU45a4w",
+    authDomain: "clientpanelprod-cb002.firebaseapp.com",
+    databaseURL: "https://clientpanelprod-cb002.firebaseio.com",
+    projectId: "clientpanelprod-cb002",
+    storageBucket: "clientpanelprod-cb002.appspot.com",
+    messagingSenderId: "1065449154786"
+  }
+};
